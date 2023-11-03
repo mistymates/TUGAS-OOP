02a742c65aa08c8686360929e7f6fc041ae1ae62
LUIS <louismorganf@gmail.com>
2023-11-04 00:21:39 +0700

    Perhitungan Nilai

--- Perhitungan Nilai Mahasiswa/src/Nilai.java
+++ Perhitungan Nilai Mahasiswa/src/Nilai.java
@@ -3,21 +3,22 @@ public class Nilai {
     String nim;
     double nilaiTubes;
     double nilaiQuiz;
-    double nilaiTugas;
-    double nilaiUTS;
-    double nilaiUAS;
+    double nilaiTugas; // <-- add semicolon here
+        double nilaiUTS;
+        double nilaiUAS;
 
-    public Nilai(String nama, String nim, double nilaiTubes, double nilaiQuiz, double nilaiTugas, double nilaiUTS, double nilaiUAS) {
-        this.nama = nama;
-        this.nim = nim;
-        this.nilaiTubes = nilaiTubes;
-        this.nilaiQuiz = nilaiQuiz;
-        this.nilaiTugas = nilaiTugas;
-        this.nilaiUTS = nilaiUTS;
-        this.nilaiUAS = nilaiUAS;
+        public Nilai(String nama, String nim, double nilaiTubes, double nilaiQuiz, double nilaiTugas, double nilaiUTS, double nilaiUAS) {
+            this.nama = nama;
+            this.nim = nim;
+            this.nilaiTubes = nilaiTubes;
+            this.nilaiQuiz = nilaiQuiz;
+            this.nilaiTugas = nilaiTugas;
+            this.nilaiUTS = nilaiUTS;
+            this.nilaiUAS = nilaiUAS;
+        }
+
+        public double hitungTotalNilai() {
+            return (0.30 * nilaiTubes) + (0.10 * nilaiQuiz) + (0.10 * nilaiTugas) + (0.25 * nilaiUTS) + (0.25 * nilaiUAS);
+        }
     }
 
-    public double hitungTotalNilai() {
-        return (0.30 * nilaiTubes) + (0.10 * nilaiQuiz) + (0.10 * nilaiTugas) + (0.25 * nilaiUTS) + (0.25 * nilaiUAS);
-    }
-}
